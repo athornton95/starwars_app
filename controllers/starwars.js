a44be70968054fa6190ceb91e130fd9b1080bc3c
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-04-17 15:08:25 -0600

    created and readed

--- controllers/starwars.js
+++ controllers/starwars.js
@@ -2,15 +2,37 @@ const express = require('express');
 const router = express.Router();
 const Starwars = require('../models/Starwars');
 
-
 // router.post('/', (req, res) => {
 // })
 
 
 router.get('/', (req, res) => {
     // res.send('reached show route');
-    res.render('index.ejs');
+    Starwars.find({}, (err, foundAllCharacters) => {
+        if(err){
+            res.send(err);
+            console.log(err);
+        } else {
+            res.render('index.ejs', {characters: foundAllCharacters});
+        }
+    })
+});
+
+router.post('/', (req, res) => {
+    Starwars.create(req.body, (err, newCharacterCreated) =>{
+        if(err){
+            res.send(err);
+            console.log(err);
+        } else {
+            console.log(newCharacterCreated);
+            res.redirect('/starwars');
+        }
+    })
 })
+
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+});
 
 
 
